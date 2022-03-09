f21b52a30816c54e2f9c6019b6504ccd541bc0a4
AtobaAzul <atoba.azul.radical@gmail.com>
2022-03-09 14:52:07 -0300

    Update modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -1,3 +1,39 @@
 local require = GLOBAL.require
 
 PrefabFiles = require("industrialrev_prefabs")
+
+
+--All the stuff for organization
+
+--strings & desc.
+modimport("init/init_names")
+modimport("init/init_bonusdescriptors")
+modimport("init/init_descriptions/generic")
+modimport("init/init_descriptions/willow")
+modimport("init/init_descriptions/wolfgang")
+modimport("init/init_descriptions/wendy")
+modimport("init/init_descriptions/wx78")
+modimport("init/init_descriptions/wickerbottom")
+modimport("init/init_descriptions/woodie")
+modimport("init/init_descriptions/wes")
+modimport("init/init_descriptions/waxwell")
+modimport("init/init_descriptions/wathgrithr")
+modimport("init/init_descriptions/webber")
+modimport("init/init_descriptions/winona")
+modimport("init/init_descriptions/wortox")
+modimport("init/init_descriptions/wormwood")
+modimport("init/init_descriptions/warly")
+modimport("init/init_descriptions/wurt")
+modimport("init/init_descriptions/walter")
+modimport("init/init_descriptions/wanda")
+
+--assets
+modimport("init/init_assets")
+modimport("init/init_widgets")
+modimport("init/minimap_icons")
+
+modimport("init/init_tuning")
+modimport("init/init_postinit")
+modimport("init/init_strings")
+modimport("init/init_actions")
+modimport("init/init_containers")
